8f43b539d95cac26b3f8f68e27e1271cd6b16af6
Linas <66268230+linas92@users.noreply.github.com>
2021-04-01 20:34:58 +0300

    Add project files.

---
+++ 2021_04_01_Linq_Hell/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2021_04_01_Linq_Hell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int RandomNumbers100 = rnd.Next(-100, 100);
+
+
+            Console.WriteLine("Goodbye Cruel World!");
+            Console.WriteLine("---------------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
